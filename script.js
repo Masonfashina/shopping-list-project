d74673b8e6c9dedeb9748fceb4885ac2c0a6615a
Mason <Masonfashina@icloud.com>
2023-11-30 21:23:11 +0100

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const ItemForm = document.getElementById('item-form');
+const ItemInput = document.getElementById('item-input');
+const ItemList = document.getElementById('item-list');
+
+//Event listeners
+
