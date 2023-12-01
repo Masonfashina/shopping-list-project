a0b592765e46db02d0350334d74d6b69089455d0
Mason <Masonfashina@icloud.com>
2023-12-01 01:41:36 +0100

    added dynamism to app

--- script.js
+++ script.js
@@ -1,68 +1,92 @@
 //adding items to list
 
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
-const clearBtn = document.getElementById('clear');
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
+function addItem(e) {
+  e.preventDefault();
 
-function addItem(e){
-    e.preventDefault();
+  const newItem = itemInput.value;
 
-    const newItem = itemInput.value
+  //validate input
 
-    //validate input
+  if (newItem === "") {
+    alert("Please enter an item");
+    return;
+  }
 
-    if (newItem === '') {
-        alert("Please enter an item");
-        return;
-    }
+  //create list item
 
-    //create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
 
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(newItem));
-   
-    const button = createButton('remove-item btn-link text-red')
-    li.appendChild(button)
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
 
-    itemList.appendChild(li)
-    itemInput.value = ''
+  //add li to DOM
+
+  itemList.appendChild(li);
+  checkUI();
+  itemInput.value = "";
 }
-function createButton(classes){
-    const button = document.createElement('button');
-    button.className = classes;
-    const icon = createIcon('fa-solid fa-xmark')
-    button.appendChild(icon);
-    return button;
-}
-
-function createIcon(classes){
-    const icon = document.createElement('i')
-    icon.className = classes;
-    return icon;
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
 }
 
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
 
 //Event listeners
 
-
 //deleting items from list
 
-function removeItem (e){
-    if (e.target.parentElement.classList.contains('remove-item')){
-        e.target.parentElement.parentElement.remove()
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+
+      checkUI()
     }
+  }
 }
 
 //clear all btn
 
-function ClearItems(e){
-while (itemList.firstChild){
-    itemList.removeChild(itemList.firstChild)
+function ClearItems(e) {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+
 }
+checkUI()
 }
 
-itemForm.addEventListener('submit',addItem)
-itemList.addEventListener('click',removeItem)
-clearBtn.addEventListener('click',ClearItems)
+//hiding filter and clear elements to make app dynamic
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+//Event Listeners
+
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", ClearItems);
+
+checkUI();
