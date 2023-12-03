e37328056bd9b0e8f04193494129a706b05d05bf
Mason <Masonfashina@icloud.com>
2023-12-03 21:56:48 +0100

    edit mode done

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function onClickItem(e) {
 function setItemToEdit(item){
   isEditMode = true
 
-  
+  itemList.querySelectorAll('li').forEach(i => i.classList.remove('edit-mode'))
 
   item.classList.add('edit-mode');
   formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
