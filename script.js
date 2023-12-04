18879a0a6b8a938b721e5548c230b400d0ab441e
Mason <Masonfashina@icloud.com>
2023-12-04 12:59:01 +0100

    tweak

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove("edit-mode");
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert("That item already exists!");
+      return;
+    }
   }
 
   //Create item DOM element
@@ -104,6 +109,12 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
